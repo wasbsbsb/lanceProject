0b7f3579285e9c8cdebc9e6ed81225b1c88715b2
wasbsbsb <2368004467@qq.com>
2023-03-22 09:03:27 +0800

    add homework

--- homework/src/pages/home/index.js
+++ homework/src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Avatar, Switch } from 'antd'
+import { Row, Col, Avatar, Switch, Radio } from 'antd'
 import router from 'umi/router';
 import { connect } from 'dva';
 import sty from './home.less'
@@ -9,19 +9,98 @@ import LoadMoreList from './components/list'
 
 class Home extends React.Component {
 
-  onChange(checked) {
-    console.log(`switch to ${checked}`);
+  state = {
+    quetionList: [
+      {
+        quetion: 'FWFWFWFWFW1 废物费？',
+        answer: 'A',
+        answerA: "fwfw12",
+        answerB: "fwfw13",
+        answerC: "fwfw14",
+      },
+      {
+        quetion: 'FWFWFWFWFW2  fwfw？',
+        answer: 'b',
+        answerA: "fwfw22",
+        answerB: "fwfw23",
+        answerC: "fwfw24",
+      },
+      {
+        quetion: 'FWFWFWFWFW 3 fwfw？',
+        answer: 'b',
+        answerA: "fwfw22",
+        answerB: "fwfw23",
+        answerC: "fwfw24",
+      },
+      {
+        quetion: 'FWFWFWFWFW4  fwfw？',
+        answer: 'b',
+        answerA: "fwfw22",
+        answerB: "fwfw23",
+        answerC: "fwfw24",
+      }, {
+        quetion: 'FWFWFWFWFW  5fwfw？',
+        answer: 'b',
+        answerA: "fwfw22",
+        answerB: "fwfw23",
+        answerC: "fwfw24",
+      }
+    ],
+    radioValue: undefined,
+    count: 0
+  }
+
+  onChange = (e) => {
+    const { count } = this.state
+
+    this.setState({ radioValue: e.target.value })
+
+    setTimeout(() => {
+      this.setState({ count: count + 1, radioValue: undefined })
+    }, 2000)
+
+
+    console.log(`switch to ${e.target.value}`);
   }
 
 
   render() {
-
+    const { quetionList, count, radioValue } = this.state
+    const { quetion, answerA, answerB, answerC } = quetionList[count]
 
 
 
     return <Row className={sty.home}>
 
-      <LoadMoreList />
+
+      <Row className={sty.show}>
+        <Col>
+        </Col>
+      </Row>
+      <Row className={sty.quetionForm}>
+
+        <Col className={sty.quetion}>
+          {quetion}
+        </Col>
+
+        <Col>
+          <Radio.Group value={radioValue} style={{ width: '100%' }} onChange={this.onChange}>
+            <Row>
+              <Col span={24}>
+                <Radio value="A">{answerA}</Radio>
+              </Col>
+              <Col span={24}>
+                <Radio value="B">{answerB}</Radio>
+              </Col>
+              <Col span={24}>
+                <Radio value="C">{answerC}</Radio>
+              </Col>
+            </Row>
+          </Radio.Group>
+        </Col>
+      </Row>
+
+      {/* <LoadMoreList /> */}
 
     </Row>
   }
