b07ad446d035a6d3c4731325cc12415d1b3fa37a
wasbsbsb <2368004467@qq.com>
2023-09-07 14:14:35 +0800

    add take photo v1

--- Vue3/vue-project/src/views/AboutView.vue
+++ Vue3/vue-project/src/views/AboutView.vue
@@ -1,63 +1,78 @@
 <template>
   <div id="add-new">
-  <h1>{{ message }}</h1>
+    <h1>{{ message }}</h1>
 
-<div>
-  <button @click="saveData">保存数据</button>
-  <button @click="readData()">读取数据</button>
-  <button v-on:click="deleteData()">删除数据</button>
-  <button v-on:click="deleteAllData()">清空数据</button>
-</div>
+    <button @click='getIPAddress'>少时诵诗书少时诵诗书</button>
 
-     <div> <el-input v-model="input" placeholder="Please input" clearable /></div>
+    <!-- <div>
+      <button @click="saveData">保存数据</button>
+      <button @click="readData()">读取数据</button>
+      <button v-on:click="deleteData()">删除数据</button>
+      <button v-on:click="deleteAllData()">清空数据</button>
+    </div>
+
+    <div> <el-input v-model="input" placeholder="Please input" clearable /></div> -->
 
   </div>
 </template>
 <script>
+
+import axios from 'axios'
+
 export default {
   name: 'AddNew',
 
   data() {
-      return {
-          message: '首页',
-          newItem: '',
-          input: '',
+    return {
+      message: '首页',
+      newItem: '',
+      input: '',
 
-      }
+    }
   },
 
 
-    methods: {
-      // 存
-      saveData() {
-          // console.log(21321)
-          let p = { name: '1231231', age: 18 }
+  methods: {
 
-          window.sessionStorage.setItem("msg", "hello!!")
-          sessionStorage.setItem("msg2", 666)
-          window.sessionStorage.setItem("person", JSON.stringify(p))
-      },
-      // 获取数据
-      readData() {
-          console.log(21321)
-          console.log(sessionStorage.getItem('msg'))
 
-          const result = sessionStorage.getItem("person")
-          this.input = result
-          console.log(JSON.parse(result))
-      },
-      // 删除
-      deleteData() {
-          let aa = sessionStorage.removeItem("msg")
-          this.input = aa
+    getIPAddress() {
+      const url = 'http://api.ipify.org?format=json';
+      axios.get(url).then(response => {
+        this.ipAddress = response.data.ip;
+      });
+    },
 
-      },
-      // 清空
-      deleteAllData() {
-          let bb = sessionStorage.clear()
-          this.input = bb
 
-      },
+    // 存
+    saveData() {
+      // console.log(21321)
+      let p = { name: '1231231', age: 18 }
+
+      window.sessionStorage.setItem("msg", "hello!!")
+      sessionStorage.setItem("msg2", 666)
+      window.sessionStorage.setItem("person", JSON.stringify(p))
+    },
+    // 获取数据
+    readData() {
+      console.log(21321)
+      console.log(sessionStorage.getItem('msg'))
+
+      const result = sessionStorage.getItem("person")
+      this.input = result
+      console.log(JSON.parse(result))
+    },
+    // 删除
+    deleteData() {
+      let aa = sessionStorage.removeItem("msg")
+      this.input = aa
+
+    },
+    // 清空
+    deleteAllData() {
+      let bb = sessionStorage.clear()
+      this.input = bb
+
+    },
 
   }
 }
